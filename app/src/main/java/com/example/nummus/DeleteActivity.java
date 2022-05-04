5c41faedb93b0a023db2a7f3348cd96fef9a2d13
NISHANTH RAIKAR <nishanth.raikar213@gmail.com>
2022-05-04 11:08:13 +0200

    initial commit

---
+++ app/src/main/java/com/example/nummus/DeleteActivity.java
@@ -0,0 +1,34 @@
+package com.example.nummus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class DeleteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delete);
+        EditText doT;
+        Button delete;
+        DBHelper DB = new DBHelper(this);
+        delete = findViewById(R.id.btndel);
+        doT = findViewById(R.id.doTdel);
+
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String dotTXT = doT.getText().toString();
+                Boolean checkudeletedata = DB.deletedata(dotTXT);
+                if(checkudeletedata==true)
+                    Toast.makeText(DeleteActivity.this, "Entry Deleted", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(DeleteActivity.this, "Entry Doesn't Exist", Toast.LENGTH_SHORT).show();
+            }        });
+    }
+}
