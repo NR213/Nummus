1a1df4ade11e292b55b121794e6fa0d61466cd8d
NISHANTH RAIKAR <nishanth.raikar213@gmail.com>
2022-05-06 00:02:07 +0200

    Added Login and Registration function

---
+++ app/src/main/java/com/example/nummus/loginactivity.java
@@ -0,0 +1,57 @@
+package com.example.nummus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class loginactivity extends AppCompatActivity {
+    EditText username, password;
+    Button btnlogin, btnregister;
+    DBHelper DB;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loginactivity);
+        username = (EditText) findViewById(R.id.txtuser);
+        password = (EditText) findViewById(R.id.txtpass);
+        btnlogin = (Button) findViewById(R.id.btnsignin);
+        btnregister = (Button) findViewById(R.id.btnregister);
+        DB = new DBHelper(this);
+
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String user = username.getText().toString();
+                String pass = password.getText().toString();
+
+                if(user.equals("")||pass.equals(""))
+                    Toast.makeText(loginactivity.this, "Please enter all the fields", Toast.LENGTH_SHORT).show();
+                else{
+                    Boolean checkuserpass = DB.checkusernamepassword(user, pass);
+                    if(checkuserpass==true){
+                        Toast.makeText(loginactivity.this, "Sign in successful", Toast.LENGTH_SHORT).show();
+                        Intent intent  = new Intent(getApplicationContext(), Front_Page.class);
+                        startActivity(intent);
+                    }else{
+                        Toast.makeText(loginactivity.this, "Invalid Credentials", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
+
+        btnregister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent  = new Intent(getApplicationContext(), registrationactivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/nummus/registrationactivity.java
@@ -0,0 +1,66 @@
+package com.example.nummus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class registrationactivity extends AppCompatActivity {
+    EditText username, password, repassword;
+    Button signup;
+    DBHelper DB;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrationactivity);
+        username = (EditText) findViewById(R.id.txtreguser);
+        password = (EditText) findViewById(R.id.txtregpass1);
+        repassword = (EditText) findViewById(R.id.txtregpass2);
+        signup = (Button) findViewById(R.id.btnsignup);
+
+        DB = new DBHelper(this);
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String user = username.getText().toString();
+                String pass = password.getText().toString();
+                String repass = repassword.getText().toString();
+
+                if(user.equals("")||pass.equals("")||repass.equals(""))
+                    Toast.makeText(registrationactivity.this, "Please enter Username and Password", Toast.LENGTH_SHORT).show();
+                else{
+                    if(pass.equals(repass)){
+                        Boolean checkuser = DB.checkusername(user);
+                        if(checkuser==false){
+                            if(Patterns.EMAIL_ADDRESS.matcher(user).matches()) {
+                                Boolean insert = DB.insertData(user, pass);
+
+                            if(insert==true){
+                                Toast.makeText(registrationactivity.this, "Registered successfully", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(getApplicationContext(),Front_Page.class);
+                                startActivity(intent);
+                            }else{
+                                Toast.makeText(registrationactivity.this, "Registration failed", Toast.LENGTH_SHORT).show();
+                            }
+                        }else{
+                                Toast.makeText(registrationactivity.this, "Invalid Email address", Toast.LENGTH_SHORT).show();
+                        }
+                        }
+                        else{
+                            Toast.makeText(registrationactivity.this, "User already exists! please sign in", Toast.LENGTH_SHORT).show();
+                        }
+                    }else{
+                        Toast.makeText(registrationactivity.this, "Passwords not matching", Toast.LENGTH_SHORT).show();
+                    }
+                } }
+        });
+
+    }
+}
+
